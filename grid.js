d49a5ee3f88cb2fca586f4c420685c80fa97ceca
p13 <p13lgst@gmail.com>
2018-11-06 13:46:34 -0200

    Fixed the major problems and now it's workikng.

--- grid.js
+++ grid.js
@@ -1,7 +1,7 @@
 function Grid(x, y, isOn) {
     this.x = x;
     this.y = y;
-    
+
     this.on = isOn;
     this.next = this.on;
 }
@@ -12,27 +12,31 @@ Grid.prototype.show = function() {
     } else {
         fill(255);
     }
-    
+
     stroke(0);
     strokeWeight(1);
-    
+
     rect(this.x, this.y, gw, gw);
-    
+
     this.on = this.next;
 }
 
 Grid.prototype.update = function(grid) {
     var neighbors = 0;
-    
+
     for (var i = -1; i <= 1; i ++) {
         for (var j = -1; j <= 1; j ++) {
-            neighbors += grid[this.x / gw + i][this.y / gw + j].on;
+            try {
+                neighbors += grid[this.x / gw + i][this.y / gw + j].on;
+            } catch (e) {
+                // Don't stop with error.
+            }
         }
     }
-    
+
     neighbors -= this.on;
-    
-    if (this.on <= 1 && (neighbors == 0 || neighbors >= 4)) {
+
+    if (this.on === 1 && (neighbors <= 1 || neighbors >= 4)) {
         this.next = 0;
     } else if (this.on === 0 && neighbors === 3) {
         this.next = 1;
--- sketch.js
+++ sketch.js
@@ -5,30 +5,40 @@ function setup() {
     background(255);
     createCanvas(600, 600);
     console.log(grid);
-    
+
     for (var x = 0; x * gw < width; x ++) {
         grid[x] = [];
     }
-    
+
     for (var x = 0; x * gw < width; x ++) {
         for (var y = 0; y * gw < height; y ++) {
             grid[x][y] = new Grid(x * gw, y * gw, 0);
         }
     }
-    for (var i = 0; i < 100; i ++) {
+    for (var i = 0; i < 1200; i ++) {
         var ranx = random(1, grid.length);
         var rany = random(1, grid[floor(ranx)].length);
-        
+
         grid[floor(ranx)][floor(rany)].on = 1;
         console.log(grid[floor(ranx)][floor(rany)].on);
     }
 }
 
 function draw() {
-    for (var x = 1; x < grid.length - 1; x ++) {
-        for (var y = 1; y < grid[x].length - 1; y ++) {
-            grid[x][y].show();
+
+    for (var x = 0; x < grid.length; x ++) {
+        for (var y = 0; y < grid[x].length; y ++) {
             grid[x][y].update(grid);
         }
     }
+
+    for (var x = 0; x < grid.length; x ++) {
+        for (var y = 0; y < grid[x].length; y ++) {
+            grid[x][y].show();
+        }
+    }
+
+    stroke(0);
+    noFill();
+    rect(0, 0, height -1, width -1);
 }
